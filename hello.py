8d0ed4268dd8f3d8b13467d66823f2b252f027ec
Isha Joshi <ishajoshi43@gmail.com>
2025-06-11 14:45:24 -0400

    Set Up Website Skeleton

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from markupsafe import escape
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
